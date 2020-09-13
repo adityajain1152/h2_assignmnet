db5aa22608a5b6bbb76304964fd31f2e1fa2f9ba
adityajain1152 <adityajain1152@gmail.com>
2020-09-13 10:34:38 +0530

    frontend validation

--- assignment-h2/src/app/register/register.component.ts
+++ assignment-h2/src/app/register/register.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  x
   constructor() { }
 
   ngOnInit(): void {
   }
+  validateForm() {
+    var x =  document.forms["register"]["fname"].value;
+    var y =  document.forms["register"]["rollno"].value;
+    var z =  document.forms["register"]["mobileno"].value;
+
+    if(z.length!=10){
+      alert('Mobile no. should be of 10 digits');
+      return false;
+    }
+
+    if(y.length!=9){
+      alert('Roll no. should be of 9 digits');
+      return false;
+    }
+    
+  }
+
 
 }
